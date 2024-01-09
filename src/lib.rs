98c5b0733a0b57a36aaa4e7006f156ef34354c2f
p6nj <breee@duck.com>
2024-01-09 23:56:43 +0100

    first sketches

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,29 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+trait Symbols {
+    type Output;
+    type Env;
+    fn zap(self) -> Self::Output;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+trait Symbol<S>
+where
+    S: Symbols,
+{
+    fn zap(self, env: S::Env, next: S) -> (S::Output, (S::Env, S));
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl<S, S1, I, O, E> Symbols for S
+where
+    S: IntoIterator<Item = S1, IntoIter = I>,
+    S1: Symbol,
+{
+    type Output = O;
+    type Env = E;
+    fn zap(self) -> Self::Output {
+        if let Ok(next) = self.into_iter().next() {
+            next.zap()
+        }
     }
 }
+
+// TODO: turn that into a derive macro
+// https://doc.rust-lang.org/reference/procedural-macros.html#derive-macros
