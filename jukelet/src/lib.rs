ca0b56c2151473d231ea49c4db4cd6b6b387e9d2
p6nj <breee@duck.com>
2024-01-11 00:24:38 +0100

    proc macro setup

--- jukelet/src/lib.rs
+++ jukelet/src/lib.rs
@@ -11,19 +11,5 @@ where
     fn zap(self, env: S::Env, next: S) -> (S::Output, (S::Env, S));
 }
 
-impl<S, S1, I, O, E> Symbols for S
-where
-    S: IntoIterator<Item = S1, IntoIter = I>,
-    S1: Symbol,
-{
-    type Output = O;
-    type Env = E;
-    fn zap(self) -> Self::Output {
-        if let Ok(next) = self.into_iter().next() {
-            next.zap()
-        }
-    }
-}
-
 // TODO: turn that into a derive macro
 // https://doc.rust-lang.org/reference/procedural-macros.html#derive-macros
