d1588a15a850fb9aec1cac21e6fbaf00577fb41d
p6nj <breee@duck.com>
2024-01-12 12:11:45 +0100

    mutable env & "where" constraints

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,25 @@
 trait Symbol<E, O> {
-    fn zap(self, env: E) -> (O, E);
+    fn zap(self, env: &mut E) -> O;
 }
 
 struct Symbols<E, O, S: Symbol<E, O>, I, C: IntoIterator<Item = S, IntoIter = I>>(C);
 
-impl<
-        E,
-        O,
-        S: Symbol<E, O>,
-        I1,
-        C1: IntoIterator<Item = S, IntoIter = I1>,
-        I2,
-        C2: IntoIterator<Item = O, IntoIter = I2>,
-    > Symbols<E, O, S, I1, C1>
+impl<E, O, S, I1, C1, I2, C2> Symbols<E, O, S, I1, C1>
+where
+    S: Symbol<E, O>,
+    I1: Iterator<Item = S>,
+    C1: IntoIterator<Item = S, IntoIter = I1>,
+    I2: Iterator<Item = O>,
+    C2: IntoIterator<Item = O, IntoIter = I2>,
 {
     pub fn zap(self) -> C2 {}
-    fn zapr(self, out: O, env: E) -> (O, E) {
+    fn zapr(self, out: C2, env: &mut E) -> C2 {
         let i = self.0.into_iter();
         if let Ok(e) = i.next() {
-            let next = e.zap(env);
-            next.0.into_iter().chain(self.zapr(out, next.1))
+            let next = e.zap(&mut env);
+            next.0.into_iter().chain(self.zapr(out, env))
         } else {
-            (out, env)
+            out
         }
     }
 }
